e967644390a530450079314d11410cb47ce24988
gantrol <gantrol.h@qq.com>
2023-03-16 16:24:53 +0800

    modified: UI 显示字符数量与文字长度移到右边

--- components/PromptPreview.svelte
+++ components/PromptPreview.svelte
@@ -1,5 +1,6 @@
 <script>
     import {tokenCount} from "~utils/prompt/tokenize";
+    import MarkdownToHTML from "~components/MarkdownToHTML.svelte";
 
     export let preview;
     let isOpen = false;
@@ -7,21 +8,17 @@
 
 <div tabindex="0" class="collapse collapse-{isOpen ? 'open': 'close'} collapse-arrow border bg-base-100">
     <input type="checkbox" bind:checked={isOpen}/>
-    <div class="collapse-title text-lg font-medium">
-        <h3>预览</h3>
+    <div class="collapse-title font-medium
+                flex justify-between items-center">
+        <div class="text-xl">预览</div>
+        <div>
+            <p>token数量：{tokenCount(preview)}</p>
+            <p>文字长度：{preview.length}</p>
+        </div>
     </div>
     <div class="collapse-content" on:dbclick={() => isOpen = !isOpen}>
-        <article class="prose bg-white-100 pt-6 pb-6">
-            <div>
-                <!--show tokens count and tokens and token group text...-->
-                <div>
-<!--                    TODO: 剩余计算，如剩余xxtoken约xx汉字，xx英文-->
-                    <div class="text-base font-medium">String Length: {preview.length}</div>
-                    <div class="text-base font-medium">Tokens Count: {tokenCount(preview)}</div>
-                    <div class="text-base font-medium max-h-36 overflow-y-auto">Tokens Group Text: {preview}</div>
-                </div>
-            </div>
-<!--            <MarkdownToHTML text={preview}/>-->
+        <article class="bg-white-100 pt-6 pb-6">
+            <MarkdownToHTML text={preview}/>
         </article>
     </div>
 </div>
